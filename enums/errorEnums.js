7f1bde9955ebfc174a452a2c7b7c2ba81ae8b41e
kai <b10223052@yuntech.org.tw>
2019-10-05 16:34:25 +0800

    add verification and error enum

---
+++ enums/errorEnums.js
@@ -0,0 +1,11 @@
+const ACCOUNT_TOO_SHORT = '帳號至少 4 個字'
+const ACCOUNT_TOO_LONG = '帳號不能超過 16 個字'
+const PASSWORD_TOO_SHORT = '密碼至少 4 個字'
+const PASSWORD_TOO_LONG = '密碼不能超過 16 個字'
+
+module.exports = {
+	ACCOUNT_TOO_SHORT,
+	ACCOUNT_TOO_LONG,
+	PASSWORD_TOO_SHORT,
+	PASSWORD_TOO_LONG,
+}
